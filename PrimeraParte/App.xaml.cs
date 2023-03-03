2bb868b9ef9de66c2c2897f7e9c8ab25470a4f06
Andrés Guzmán <99285077+4ndr3sG@users.noreply.github.com>
2023-03-03 16:34:21 +0100

    Add files via upload

---
+++ PrimeraParte/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrimeraParte
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+    public class User
+    {
+        public string Nombre { get; set; }
+        public int Puntuacion { get; set; }
+    }
+}
